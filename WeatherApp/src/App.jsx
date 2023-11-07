d66f0eb6370b4e1e3035e117d06c82cb8619f937
deekshith.v@borderfree.io-at-297767201074 <deekshith.v@gmail.com>
2023-11-07 16:13:54 +0530

    commit 2

--- WeatherApp/src/App.jsx
+++ WeatherApp/src/App.jsx
@@ -14,12 +14,6 @@ function App() {
 
   useEffect(() => {
     axios
-      // .get("https://restcountries.com/v3.1/all")
-      // .then((response) => {
-      //   const countryOptions = response.data.map((country) => ({
-      //     value: country.cca2,
-      //     label: country.name.common,
-      //   }));
       .get("https://api.countrystatecity.in/v1/countries", {
         headers: {
           "X-CSCAPI-KEY":
@@ -39,10 +33,16 @@ function App() {
   }, []);
 
   const fetchData = () => {
+    var cityforData = "";
+    if (city == "" || city == []) {
+      cityforData = state.label;
+    } else {
+      cityforData = city.label;
+    }
     const optionsData = {
       method: "GET",
       url: "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather",
-      params: { city: city.label },
+      params: { city: cityforData },
       headers: {
         "X-RapidAPI-Key": "6829abde76mshf794d68e8387478p1c3a96jsne68bccde7065",
         "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
@@ -59,69 +59,59 @@ function App() {
       });
   };
 
-  const callStates = (e) => {
-    console.log(e,country);
-    if (country) {
+  useEffect(() => {
+    console.log(country.value);
+    if (country != "") {
       axios
-        // .get(`https://api.apilayer.com/geo/country/cities/${country.value}`, {
-        //   redirect: "follow",
-        //   headers: {
-        //     apikey: "niWCA7alvujSoKYPM6Zv1NoYkYUuNy18",
-        //   },
-        // })
-        // .then((response) => {
-        //   const cityOptions = response.data.map((result) => ({
-        //     value: result.name,
-        //     label: result.name,
-        //   }));
-        //   setCityOptions(cityOptions);
-        //   console.log(cityOptions);
-        // })
-        .get(`https://api.countrystatecity.in/v1/countries/${country.value}/states`, {
-        headers: {
-          "X-CSCAPI-KEY":
-            "NWhrU2FIakxibzljVDZFOXJKdktvR3VsWDFSYXk0ZmtqaVl6NWxDZA==",
-        },
-      })
-      .then((response) => {
-        const stateOptions = response.data.map((state) => ({
-          value: state.iso2,
-          label: state.name,
-        }));
-        console.log(stateOptions);
-        setStateOptions(stateOptions);
-      })
+        .get(
+          `https://api.countrystatecity.in/v1/countries/${country.value}/states`,
+          {
+            headers: {
+              "X-CSCAPI-KEY":
+                "NWhrU2FIakxibzljVDZFOXJKdktvR3VsWDFSYXk0ZmtqaVl6NWxDZA==",
+            },
+          }
+        )
+        .then((response) => {
+          const stateOptions = response.data.map((state) => ({
+            value: state.iso2,
+            label: state.name,
+          }));
+          console.log(stateOptions);
+          setStateOptions(stateOptions);
+        })
         .catch((error) => {
           console.error("Error fetching cities:", error);
         });
     }
-  }
+  }, [country]);
 
-  const callCities = (e) => {
-    setState(e);
-    console.log("From city",country,state);
+  useEffect(() => {
     if (state != "") {
       axios
-        .get(`https://api.countrystatecity.in/v1/countries/${country.value}/states/${state.value}/cities`, {
-        headers: {
-          "X-CSCAPI-KEY":
-            "NWhrU2FIakxibzljVDZFOXJKdktvR3VsWDFSYXk0ZmtqaVl6NWxDZA==",
-        },
-      })
-      .then((response) => {
-        const cityOptions = response.data.map((city) => ({
-          value: city.id,
-          label: city.name,
-        }));
-        console.log(response);
-        setCityOptions(cityOptions);
-      })
+        .get(
+          `https://api.countrystatecity.in/v1/countries/${country.value}/states/${state.value}/cities`,
+          {
+            headers: {
+              "X-CSCAPI-KEY":
+                "NWhrU2FIakxibzljVDZFOXJKdktvR3VsWDFSYXk0ZmtqaVl6NWxDZA==",
+            },
+          }
+        )
+        .then((response) => {
+          const cityOptions = response.data.map((city) => ({
+            value: city.id,
+            label: city.name,
+          }));
+          console.log(response);
+          setCityOptions(cityOptions);
+        })
         .catch((error) => {
           console.error("Error fetching cities:", error);
         });
     }
-  };
-  console.log(country);
+  }, [state]);
+
   //const multi = country&&city
   return (
     <div className="body">
@@ -134,14 +124,13 @@ function App() {
             options={countryOptions}
             placeholder="Select Country"
             value={country}
-            onClick={(e)=>setCountry(e)}
-            onChange={callStates}
+            onChange={(e) => setCountry(e)}
           />
           <Select
             options={stateOptions}
             placeholder="Select City"
             value={state}
-            onChange={callCities}
+            onChange={(e) => setState(e)}
           />
           <Select
             options={cityOptions}
@@ -154,14 +143,9 @@ function App() {
           </button>
         </div>
         <div className="weather-info">
-          {/* {multi &&(
-            <div>
-              <h2 id="location">City : {city.value}</h2>
-            <h2 id="location">Country: {country.label}</h2>
-            </div>
-          )} */}
-          <h2 id="location">City : {city.value}</h2>
           <h2 id="location">Country: {country.label}</h2>
+          <h2 id="location">State : {state.label}</h2>
+          <h2 id="location">City : {city.label}</h2>
           <div className="weather-details">
             <img
               id="weather-icon"
@@ -179,7 +163,7 @@ function App() {
         <div className="forecast">
           <div className="forecast-item">
             <div className="day">Max Temp</div>
-            <div className="temperature">5{weatherData.max_temp}C</div>
+            <div className="temperature">{weatherData.max_temp}C</div>
           </div>
           <div className="forecast-item">
             <div className="day">Min Temp</div>
